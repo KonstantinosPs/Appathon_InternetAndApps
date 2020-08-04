3f753336c5b15365f04fe5f57f0be48c42500f05
KonstantinosPs <konpsychogios@gmail.com>
2020-08-04 18:46:02 +0300

    Updates

---
+++ precache-manifest.eb1229bea7173f02dd18079c9c02073d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e42f1a3654ccb7464fef7f6b187c9b",
+    "url": "/Appathon_InternetAndApps/index.html"
+  },
+  {
+    "revision": "ad206543557e2b3c5e1b",
+    "url": "/Appathon_InternetAndApps/static/css/main.48496612.chunk.css"
+  },
+  {
+    "revision": "255c3ad8c4b108a2d6ec",
+    "url": "/Appathon_InternetAndApps/static/js/2.1136be1b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Appathon_InternetAndApps/static/js/2.1136be1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad206543557e2b3c5e1b",
+    "url": "/Appathon_InternetAndApps/static/js/main.041ae555.chunk.js"
+  },
+  {
+    "revision": "7cb1de6a88d5d755fd3c",
+    "url": "/Appathon_InternetAndApps/static/js/runtime-main.b432538c.js"
+  }
+]);
