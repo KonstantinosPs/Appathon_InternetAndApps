68eede46054229b1d2a3d23bc2ba64b0622ac09e
KonstantinosPs <68636846+KonstantinosPs@users.noreply.github.com>
2020-08-16 18:55:07 +0300

    Update socialconnect.js

--- src/components/socialComponent/socialconnect.js
+++ src/components/socialComponent/socialconnect.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {
     faFacebook,
-    faInstagram
+    faInstagram,
+    faYoutube,
+    faGithub
 } from "@fortawesome/free-brands-svg-icons";
 import "./socialconnect.css";
 
@@ -12,10 +14,16 @@ class SocialConnect extends Component {
 
             <div className="social-container"> 
                 <h5>@KonstantinosPs</h5> 
+                <a href="https://github.com/KonstantinosPs/Appathon_InternetAndApps"
+                className="github"><FontAwesomeIcon icon={faGithub} size="2x"/>
+
+                </a>
                 <a href="https://www.facebook.com/konstantinos.psyxogios"
                 className="facebook-social"><FontAwesomeIcon icon={faFacebook} size="2x"/>
-                
-                
+                </a>
+                <a href="https://www.youtube.com/watch?v=DV1Stm0YQQ0&feature=youtu.be"
+                className="youtube-social"><FontAwesomeIcon icon={faYoutube} size="2x"/>
+
                 </a>
                 <a href="https://www.instagram.com/kwstantinos_ps/?hl=el"
                 className="instagram-social"><FontAwesomeIcon icon={faInstagram} size="2x"/>
@@ -33,4 +41,3 @@ class SocialConnect extends Component {
 }
  
 export default SocialConnect;  
-
