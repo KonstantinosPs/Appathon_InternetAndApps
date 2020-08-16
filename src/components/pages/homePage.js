916573fe75da594a8a8929f63cf4c69d9f4346c6
KonstantinosPs <68636846+KonstantinosPs@users.noreply.github.com>
2020-08-16 17:26:18 +0300

    Update homePage.js

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './homePage.css'
-
+import Logo from './images/poweredby.png';
 class Homepage extends Component {
         render() { 
         return (  
@@ -12,7 +12,7 @@ class Homepage extends Component {
                  Το input θα είναι το όνομα της ασθένειας το οποίο θα το ορίζει ο χρήστης μέσω της web σελίδας 
                  καθώς και το εύρος της χρονικής περιόδου που έλαβαν οι κλινικές μελέτες χώρα και το output θα είναι οι χώρες ταξινομημένες ανάλογα
                  με τον αριθμό των κλινικών μελετών.Η εξόρυξη των δεδομένων των κλινικών μελετών γίνεται μέσω api από την ιστοσελίδα <a href="https://clinicaltrials.gov">Clinicaltrials.gov</a></p>   
-
+                <img src={Logo} alt='' className="poweredBy"/>
             </div>
     
 
